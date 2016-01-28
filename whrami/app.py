e37466014850a9329e6ec64ab7ba1317725fdc82
Simon Cross <hodgestar@gmail.com>
2016-01-28 23:27:56 +0200

    Downgrade implementation to Python 3.4.

--- whrami/app.py
+++ whrami/app.py
@@ -1,5 +1,6 @@
 """ Whrami aiohttp app. """
 
+import asyncio
 import aiohttp
 from aiohttp import web
 from . import events
@@ -11,36 +12,44 @@ class IndexView(web.View):
         return web.Response(body=b"Hello world.")
 
 
-async def websocket_handler(request):
-    ws = web.WebSocketResponse()
-    await ws.prepare(request)
+class WebSocketView(web.View):
+    """ Websocket endpoint. """
 
-    async for msg in ws:
-        if msg.tp == aiohttp.MsgType.text:
+    @asyncio.coroutine
+    def get(self):
+        ws = web.WebSocketResponse()
+        yield from ws.prepare(self.request)
+
+        while True:
             try:
-                event = events.parse_event(msg.data)
-            except:
-                ws.send_str(events.close_web_socket.to_json())
-                await ws.close()
-                return ws
-            if event.etype == 'close_web_socket':
-                await ws.close()
-                return ws
-            if event.etype == 'user_input':
-                ws.send_str(event.to_json())  # echo to client
-                ws.send_str(events.user_output("Got it.").to_json())
-        elif msg.tp == aiohttp.MsgType.error:
-            print('websocket connection closed with exception %s' %
-                  ws.exception())
+                msg = yield from ws.receive()
+            except web.WSClientDisconnectError:
+                break
+            if msg.tp == aiohttp.MsgType.text:
+                try:
+                    event = events.parse_event(msg.data)
+                except:
+                    ws.send_str(events.close_web_socket.to_json())
+                    yield from ws.close()
+                    return ws
+                if event.etype == 'close_web_socket':
+                    yield from ws.close()
+                    return ws
+                if event.etype == 'user_input':
+                    ws.send_str(event.to_json())  # echo to client
+                    ws.send_str(events.user_output("Got it.").to_json())
+            elif msg.tp == aiohttp.MsgType.error:
+                print('websocket connection closed with exception %s' %
+                      ws.exception())
 
-    print('websocket connection closed')
-    return ws
+        print('websocket connection closed')
+        return ws
 
 
 def create_app():
     """ Return an instance of the Whrami app. """
     app = web.Application()
     app.router.add_route('*', '/', IndexView)
-    app.router.add_route('*', '/ws', websocket_handler)
+    app.router.add_route('*', '/ws', WebSocketView)
     app.router.add_static('/static/', 'whrami/static/')
     return app
